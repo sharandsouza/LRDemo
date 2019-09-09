9d1b943df383c3d3dd043f37cef6388ac9a2ddef
sharandsouza <sharandsouza87@gmail.com>
2019-09-09 09:04:08 +0530

    This is my first LR commit

---
+++ Action.c
@@ -0,0 +1,14 @@
+Action()
+{
+	return 0;
+
+	web_custom_request("web_custom_request",
+		"URL=www.example.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+}
